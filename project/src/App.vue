a7127f1e6d7c8e597097f28728fcfe2a5ff14ee8
dongxiuling <644961699@qq.com>
2020-02-25 16:28:55 +0800

    vue-cli

---
+++ project/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+      <common-header></common-header>
+      <home></home>
+      <common-footer></common-footer>
+  </div>
+</template>
+<script>
+    // 1 import 引入 2 components中声明
+    import CommonHeader from './components/CommonHeader';
+    import CommonFooter from './components/CommonFooter';
+    import Home from './views/Home'
+    export default {
+        components:{
+          CommonHeader,
+          CommonFooter,
+          Home
+        }
+    }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        babakbkab首页的内容
+        <h2>babakbkab首页的内容</h2>
+        <p>babakbkab首页的内容</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
