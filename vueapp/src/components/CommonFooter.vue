bff8de6df48d4fd21daa7b34baa6a3300bf23b31
dongxiuling <644961699@qq.com>
2020-02-27 15:03:08 +0800

    vueapp

---
+++ vueapp/src/components/CommonFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>   
+        footer
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    footer{
+        height: 1rem;
+        background: #f00;
+        line-height: 1rem;
+        position: fixed;
+        bottom: 0px;
+        width:100%;
+    }
+</style>
---
+++ vueapp/src/components/CommonHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        header
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    header{
+        background: red;
+        height:1rem;
+        line-height: 1rem;
+        text-align: center;
+        font-size: 16px;
+        position: fixed;
+        top:0px;
+        width:100%;
+    }
+</style>
