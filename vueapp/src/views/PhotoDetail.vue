df4d2f7003b96221645dfaed415e2fca4f192e4b
dongxiuling <644961699@qq.com>
2020-03-02 20:42:19 +0800

    vueapp

--- vueapp/src/views/PhotoDetail.vue
+++ vueapp/src/views/PhotoDetail.vue
@@ -1,16 +1,41 @@
 <template>
     <div>
-        <div class="img-box" 
-            :style="{backgroundImage:`url(${$store.state.photo[idx].src})`}">
-        </div>
+        <!-- <v-touch v-on:swipeleft="onSwipeLeft">Swipe me!</v-touch> -->
+        <router-link to="/photo">
+            <v-touch class="img-box" 
+                :style="{backgroundImage:`url(${$store.state.photo[idx].src})`}"
+                @swipeleft="next()"
+                @swiperight="prev()"
+            >
+            </v-touch>
+        </router-link>
     </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import VueTouch from 'vue-touch'
+    Vue.use(VueTouch, {name: 'v-touch'})
     export default {
         data(){
             return {
                 idx:this.$route.query.index
+            }
+        },
+        methods:{
+            next(){
+                // console.log('111111111');
+                // 显示下一张图
+                this.idx++;
+                if(this.idx >= this.$store.state.photo.length){
+                    this.idx = 0
+                }
+            },
+            prev(){
+                this.idx--;
+                if(this.idx <0){
+                    this.idx = this.$store.state.photo.length-1;
+                }
             }
         }
     }
